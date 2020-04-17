a9421873ed6fe4c27098910a9660fe1e38f3d690
Guðmundur Björn Birkisson <gbi@enonic.com>
2020-04-17 16:53:34 +0200

    Update user creation to handle missing claims

--- src/main/resources/lib/users.js
+++ src/main/resources/lib/users.js
@@ -30,7 +30,31 @@ function runAsSu(callback) {
 }
 
 function sanitizeUserName(userName) {
-    return userName.replace(/[|\\\/?*]/gi, '_');
+    return userName.replace(/[|@\\\/?*]/gi, '-');
+}
+
+function getUsername(idProviderConfig, payload) {
+    let userName = payload[idProviderConfig.claim_username];
+    if (userName === undefined) {
+        userName = required(payload, 'sub');
+    }
+    return sanitizeUserName(userName);
+}
+
+function getDisplayName(idProviderConfig, payload) {
+    let displayName = payload[idProviderConfig.claim_displayname];
+    if (displayName === undefined) {
+        displayName = required(payload, 'sub');
+    }
+    return displayName;
+}
+
+function getEmail(idProviderConfig, payload) {
+    let email = payload[idProviderConfig.claim_email];
+    if (email === undefined) {
+        email = getUsername(idProviderConfig, payload) + "@serviceaccount.enonic";
+    }
+    return email;
 }
 
 /**
@@ -40,26 +64,23 @@ function sanitizeUserName(userName) {
  */
 exports.login = function (payload) {
     const idProviderConfig = authLib.getIdProviderConfig();
-    const userId = sanitizeUserName(required(payload, idProviderConfig.claim_username));
+    const userName = getUsername(idProviderConfig, payload);
 
     let user = runAsSu(function () {
-        log.debug("Searching for user '%s'", userId);
-        const user = authLib.getPrincipal("user:" + portalLib.getIdProviderKey() + ":" + userId);
+        log.debug("Searching for user '%s'", userName);
+        const user = authLib.getPrincipal("user:" + portalLib.getIdProviderKey() + ":" + userName);
 
         if (user) {
-            log.debug("Found user '%s'", userId);
+            log.debug("Found user '%s'", userName);
             return user.login;
         }
 
-        const displayName = payload[idProviderConfig.claim_displayname];
-        const email = payload[idProviderConfig.claim_email];
-
-        log.debug("User '%s' not found creating...", userId);
+        log.debug("User '%s' not found creating...", userName);
         return authLib.createUser({
             idProvider: portalLib.getIdProviderKey(),
-            name: userId,
-            displayName: displayName,
-            email: email
+            name: userName,
+            displayName: getDisplayName(idProviderConfig, payload),
+            email: getEmail(idProviderConfig, payload)
         }).login;
     });
 
