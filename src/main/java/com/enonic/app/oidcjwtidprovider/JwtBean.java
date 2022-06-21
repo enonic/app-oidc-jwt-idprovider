41f5c758ab857df92575d26f568dc28473869dac
rymsha <rymsha@yandex.ru>
2022-06-21 16:11:31 +0200

    Don't use okhttp3 for JwtBean #69

--- src/main/java/com/enonic/app/oidcjwtidprovider/JwtBean.java
+++ src/main/java/com/enonic/app/oidcjwtidprovider/JwtBean.java
@@ -1,56 +1,19 @@
 package com.enonic.app.oidcjwtidprovider;
 
 import java.io.IOException;
-import java.net.URL;
-import java.time.Duration;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.auth0.jwk.GuavaCachedJwkProvider;
-import com.auth0.jwk.UrlJwkProvider;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.ResponseBody;
 
 public class JwtBean
 {
-    private static final Logger log = LoggerFactory.getLogger( JwtBean.class );
-
-    private static final ObjectMapper mapper = new ObjectMapper();
-
     private final JwtHandler jwtHandler;
 
     private final Map<String, Object> wellKnown;
 
-    public JwtBean( final String wellKnownEndpoint )
+    public JwtBean( final Map<String, Object> wellKnown, final JwtHandler jwtHandler )
         throws IOException
     {
-        log.debug( "Initializing OIDC provider" );
-        this.wellKnown = getWellKnownEndpoint( wellKnownEndpoint );
-        this.jwtHandler = new JwtHandler( new RSAAlgorithmProvider(
-            new GuavaCachedJwkProvider( new UrlJwkProvider( new URL( (String) wellKnown.get( "jwks_uri" ) ) ) ) ) );
-    }
-
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> getWellKnownEndpoint( final String wellKnownEndpoint )
-        throws IOException
-    {
-        log.debug( "Fetching well known endpoint: " + wellKnownEndpoint );
-        final Request.Builder request = new Request.Builder();
-        request.url( wellKnownEndpoint );
-        ResponseBody body = new OkHttpClient().newBuilder().
-            callTimeout( Duration.ofSeconds( 5 ) ).
-            connectTimeout( Duration.ofSeconds( 5 ) ).
-            build().
-            newCall( request.build() ).
-            execute().
-            body();
-
-        return mapper.readValue( body.bytes(), Map.class );
+        this.wellKnown = wellKnown;
+        this.jwtHandler = jwtHandler;
     }
 
     public Object getWellKnown()
--- src/main/java/com/enonic/app/oidcjwtidprovider/JwtBeanBuilder.java
+++ src/main/java/com/enonic/app/oidcjwtidprovider/JwtBeanBuilder.java
@@ -1,13 +1,28 @@
 package com.enonic.app.oidcjwtidprovider;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.auth0.jwk.JwkProviderBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
 public class JwtBeanBuilder
 {
-    private static Cache<String, JwtBean> jwtHandlerCache = CacheBuilder.newBuilder().build();
+    private static final Logger log = LoggerFactory.getLogger( JwtBeanBuilder.class );
+
+    private static final int TIMEOUT_MS = 5000;
+
+    private static final Cache<String, JwtBean> JWT_HANDLER_CACHE = CacheBuilder.newBuilder().build();
 
     private String wellKnownEndpoint;
 
@@ -19,6 +34,36 @@ public class JwtBeanBuilder
     public JwtBean build()
         throws ExecutionException
     {
-        return jwtHandlerCache.get( wellKnownEndpoint, () -> new JwtBean( wellKnownEndpoint ) );
+        return JWT_HANDLER_CACHE.get( wellKnownEndpoint, () -> newJwtBean( wellKnownEndpoint ) );
+    }
+
+    private static JwtBean newJwtBean( final String wellKnownEndpoint )
+        throws IOException
+    {
+        log.debug( "Initializing OIDC provider" );
+        var wellKnown = getWellKnownEndpoint( wellKnownEndpoint );
+        var jwkProvider = new JwkProviderBuilder( new URL( (String) wellKnown.get( "jwks_uri" ) ) ).cached( true )
+            .timeouts( TIMEOUT_MS, TIMEOUT_MS )
+            .build();
+        return new JwtBean( wellKnown, new JwtHandler( new RSAAlgorithmProvider( jwkProvider ) ) );
+    }
+
+    private static Map<String, Object> getWellKnownEndpoint( final String wellKnownEndpoint )
+    {
+        try
+        {
+            final URLConnection c = new URL( wellKnownEndpoint ).openConnection();
+            c.setConnectTimeout( TIMEOUT_MS );
+            c.setReadTimeout( TIMEOUT_MS );
+
+            try (InputStream inputStream = c.getInputStream())
+            {
+                return new ObjectMapper().readerFor( Map.class ).readValue( inputStream );
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new UncheckedIOException( e );
+        }
     }
 }
